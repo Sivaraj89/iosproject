d87a535138f0b44731c48dd4b8ab4008a5c79fe9
Wipro <wipro@ramya.local>
2018-05-24 10:18:58 +0530

    Initial Commit

---
+++ sivarajexercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sivarajexercise
+//
+//  Created by Wipro on 24/05/18.
+//  Copyright © 2018 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
